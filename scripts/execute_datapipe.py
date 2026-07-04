a3fa72160e8e93640b7e428d965462f3ec7054ba
Tejas204 <tejasdhopavkar2000@gmail.com>
2026-07-04 11:34:06 +0200

    File structure changes

--- scripts/execute_datapipe.py
+++ scripts/execute_datapipe.py
@@ -9,14 +9,14 @@ from pipeline import datapipe
 
 if __name__ == "__main__":
     datapipe_object = datapipe.DataPipeline(
-        annotations_path="/Users/tejasdhopavkar/Documents/MS/Saarland_University/Semester_3/MLU/Project/data/annotations.json",
-        annotation_distionary_path="/Users/tejasdhopavkar/Documents/MS/Saarland_University/Semester_3/MLU/Project/data/raw_metadata.json",
+        annotations_path="/Users/tejasdhopavkar/Documents/MS/Saarland_University/Semester_3/MLU/Project/data/metadata/annotations.json",
+        annotation_distionary_path="/Users/tejasdhopavkar/Documents/MS/Saarland_University/Semester_3/MLU/Project/data/metadata/raw_metadata.json",
         images_path="None"
     )
 
     datapipe_object.annotations_to_dictionary()
     datapipe_object.run_basic_analysis()
-    datapipe_object.filter_samples(remove_all_false_annotations=True, storage_path="/Users/tejasdhopavkar/Documents/MS/Saarland_University/Semester_3/MLU/Project/data/filtered_metadata.json")
-    unique_concepts = datapipe_object.extract_concepts(storage_path="/Users/tejasdhopavkar/Documents/MS/Saarland_University/Semester_3/MLU/Project/data/concepts.json")
-    datapipe_object.build_caption(storage_path="/Users/tejasdhopavkar/Documents/MS/Saarland_University/Semester_3/MLU/Project/data/captioned_metadata.json")
-    datapipe_object.build_concept_vector(unique_concepts=unique_concepts, storage_path="/Users/tejasdhopavkar/Documents/MS/Saarland_University/Semester_3/MLU/Project/data/concept_metadata.json")
+    datapipe_object.filter_samples(remove_all_false_annotations=True, storage_path="/Users/tejasdhopavkar/Documents/MS/Saarland_University/Semester_3/MLU/Project/data/metadata/filtered_metadata.json")
+    unique_concepts = datapipe_object.extract_concepts(storage_path="/Users/tejasdhopavkar/Documents/MS/Saarland_University/Semester_3/MLU/Project/data/metadata/concepts.json")
+    datapipe_object.build_caption(storage_path="/Users/tejasdhopavkar/Documents/MS/Saarland_University/Semester_3/MLU/Project/data/metadata/captioned_metadata.json")
+    datapipe_object.build_concept_vector(unique_concepts=unique_concepts, storage_path="/Users/tejasdhopavkar/Documents/MS/Saarland_University/Semester_3/MLU/Project/data/metadata/concept_metadata.json")
